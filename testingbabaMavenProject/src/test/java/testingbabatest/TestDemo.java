b24d5c0d341719f98b54789d863c2c07616c8dc9
ManitaTaank <163091904+ManitaTaank@users.noreply.github.com>
2026-05-23 12:37:38 +0530

    Add files via upload

---
+++ testingbabaMavenProject/src/test/java/testingbabatest/TestDemo.java
@@ -0,0 +1,26 @@
+package testingbabatest;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestDemo {
+
+	@Test
+	public void launchURL() {
+
+		System.setProperty(
+			"java.util.logging.config.file",
+			"src/test/resources/logging.properties"
+		);
+
+		WebDriverManager.chromedriver().setup();
+
+		ChromeDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+
+		driver.get("https://google.com");
+	}
+}
